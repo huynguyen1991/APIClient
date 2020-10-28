f9122315913d774f0280626d3f08a0e17bedba5c
huy <huy.nguyen1@ntq-solution.com.vn>
2020-10-28 15:47:54 +0700

    first commit

---
+++ APIClient/APIClient.h
@@ -0,0 +1,18 @@
+//
+//  APIClient.h
+//  APIClient
+//
+//  Created by NTQ on 10/28/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for APIClient.
+FOUNDATION_EXPORT double APIClientVersionNumber;
+
+//! Project version string for APIClient.
+FOUNDATION_EXPORT const unsigned char APIClientVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <APIClient/PublicHeader.h>
+
+
